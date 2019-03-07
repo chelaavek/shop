c7456ee618d96edd4a505949a735e45b745ce81d
chelaavek <GlebK199972@gmail.com>
2019-03-07 21:02:22 +0300

    'Validation'

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :title, :description, presence: true
+  validates_numericality_of :price , {:greater_than_or_equal_to => 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|jpeg|png)\Z}i,
+      message: 'URL должен указывать на изображение формата GIF, JPG, JPEG или PNG.'
+  }
 end
