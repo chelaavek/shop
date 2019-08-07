ed5021ed01841c636a183b788c4b9d4f7029cb77
chelaavek <GlebK199972@gmail.com>
2019-08-07 13:45:06 +0300

    activeadmin, status, links

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
-  validates :title, :description, presence: true
+  validates :title, presence: true
   validates_numericality_of :price , {:greater_than_or_equal_to => 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
       with: %r{\.(gif|jpg|jpeg|png)\Z}i,
       message: 'URL должен указывать на изображение формата GIF, JPG, JPEG или PNG.'
   }
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.delete_all
-Product.create!(title: 'Programming Ruby 1.9 & 2.0', description:%{<p>Ruby is nive.</p>}, image_url: 'runy.jpg', price: 49.95)
+Product.create!(title: 'Programming Ruby 1.9 & 2.0', description:%{<p>Ruby is nive.</p>}, image_url: 'runy.jpg', price: 49.95)AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
