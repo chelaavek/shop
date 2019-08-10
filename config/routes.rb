8e47d1e69a64ba9130e5b3944b34bd8e36da58c9
chelaavek <GlebK199972@gmail.com>
2019-08-10 12:55:09 +0300

    dynamic login button, redirects

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'store#index', as: 'store'
+  namespace :admin_user do
+    root to: 'store#index'
+  end
 end
